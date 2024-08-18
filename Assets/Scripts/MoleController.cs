6eeedf84c522397e40515ca75a739bd49a5fd931
Giacomo C. <giacomo.corso@gmail.com>
2024-08-18 18:58:23 +0200

    mole comes out of the hole

--- Assets/Scripts/MoleController.cs
+++ Assets/Scripts/MoleController.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class MoleController : MonoBehaviour
 {
+
+    private int velocity;
+    private float starterTime;
+    private bool isOut;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.localPosition = new Vector3(10.45f, 0, -23);
+        velocity = 3;        
+        isOut = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (transform.localPosition.y < 2.25f && isOut == false)
+        {
+            transform.Translate(Time.deltaTime * velocity * Vector3.up);
+        }
+
+        if (transform.localPosition.y >= 2.25f && isOut == false)
+        {
+            isOut = true;
+            starterTime = Time.time;
+            Debug.Log(starterTime);
+        }
     }
 }
