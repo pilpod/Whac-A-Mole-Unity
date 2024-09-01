718d59678c5d222f10dac829990011d7107102a9
Giacomo C. <giacomo.corso@gmail.com>
2024-09-01 17:00:22 +0200

    hammer working fine. Feature done

--- Assets/Scripts/HammerController.cs
+++ Assets/Scripts/HammerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
@@ -6,11 +7,13 @@ using UnityEngine;
 public class HammerController : MonoBehaviour
 {
     [SerializeField] private Camera mainCamera;
+    private bool isUp = true;
+    private float maxTimeDown = 0.25f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -18,22 +21,50 @@ public class HammerController : MonoBehaviour
     {
         Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
 
+        // Hammer fallow the mouse
         if (Physics.Raycast(ray, out RaycastHit hit))
         {
             transform.position = hit.point;
         }
 
-        if (Input.GetMouseButtonDown(0))
+        // Hammer swing down when mouse button left is clicked
+        if (Input.GetMouseButtonDown(0) && isUp)
         {
-            Vector3 rotation = new Vector3(0, 0, 45);
-            transform.Rotate(rotation);
+            SwingDown();
         }
 
-        if (Input.GetMouseButtonUp(0))
+        // Hammer return to initial position if left button was up
+        if (Input.GetMouseButtonUp(0) && !isUp)
         {
-            Vector3 rotation = new Vector3(0, 0, -45);
-            transform.Rotate(rotation);
+            SwingUp();
         }
+
+        // Hammer cannot be down for more then half a second
+        if (!isUp)
+        {
+            maxTimeDown -= Time.deltaTime;
+
+            if (maxTimeDown <= 0)
+            {
+                SwingUp();
+            }
+        }
+
+    }
+
+    private void SwingDown()
+    {
+        Vector3 rotation = new Vector3(0, 0, 45);
+        transform.Rotate(rotation);
+        isUp = false;
+    }
+
+    private void SwingUp()
+    {
+        Vector3 reverseRotation = new Vector3(0, 0, -45);
+        transform.Rotate(reverseRotation);
+        isUp = true;
+        maxTimeDown = 0.25f;
     }
 
     private void OnTriggerEnter(Collider other)
